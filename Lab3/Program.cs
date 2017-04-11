01fdb57766944e03339daaf10240e138f937ee15
alsoraimi <alsoraimi46@gmail.com>
2017-04-11 16:31:44 -0400

    added integers and values

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -11,11 +11,19 @@ namespace Lab3
         static void Main(string[] args)
         {
             Console.WriteLine("Learn your squares and cubes!!");
+            Console.WriteLine("Enter an integer");
+
 
             int num;
             int squ;
             int cub;
-            num = Console.ReadLine.Parse()
+
+            num = int.Parse(Console.ReadLine());
+            squ = num * num;
+            cub = num * num * num;
+
+
+            
     
 
 

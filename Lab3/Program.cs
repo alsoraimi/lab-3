4d1304e5e11dbb0a7cd7a4c3927c5f7ac33a30e7
alsoraimi <alsoraimi46@gmail.com>
2017-04-11 16:16:25 -0400

    first commit

---
+++ Lab3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learn your squares and cubes!!");
+
+            int num;
+            int squ;
+            int cub;
+            num = Console.ReadLine.Parse()
+    
+
+
+        }
+    }
+}

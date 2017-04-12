d463e3792c3a8d8d8b9be1121bef57276f41a285
alsoraimi <alsoraimi46@gmail.com>
2017-04-12 13:13:18 -0400

    add 2 methods for cube and squared

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -10,23 +10,82 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Learn your squares and cubes!!");
-            Console.WriteLine("Enter an integer");
+
+            bool askToContinue = false;
+            do
+            {
+                Console.WriteLine("Learn your squares and cubes!!");
+                
 
 
-            int num;
-            int squ;
-            int cub;
-
-            num = int.Parse(Console.ReadLine());
-            squ = num * num;
-            cub = num * num * num;
+                int num;
 
 
-            
-    
+                do
+                {
+                    Console.WriteLine("Enter an integer");
+                    num = int.Parse(Console.ReadLine());
+
+
+                    Console.WriteLine("Number    Squared    Cubed\n======    =======    =====");     //format table
+
+                    for (int i = 1; i <= num; i++)
+                    {
+                        Console.WriteLine("{0,0}{1,10}{2,11}", i, Square(i), Cube(i));
+
+
+                    }
+                    Console.WriteLine("do you wish to contine? " + "Y/N");
+                    if (Console.ReadLine().ToUpper() == ("Y"))
+                        askToContinue = true;
+                    else
+                    {
+                        return;
+                    }
+
+                    //if (num < 0)
+                    //{
+                    //    Console.WriteLine("please enter number equal to or higher than 0");
+                    //}
+
+                    //if (!int.TryParse(input, out num))
+                    //{
+                    //    Console.WriteLine("Not an integer");
+                    //}
+
+
+
+                } while (askToContinue);
+            } while (true);
+
+        }
+
+
+ //seperate methods for square and cube
+
+    public static int Square(int n)
+
+
+        {
+            int square = n * n;
+            return square;
+        }
+
+        public static int Cube (int n)
+
+        {
+            int cube = n * n * n;
+            return cube;
+
+
+        }
+
+       
 
 
         }
     }
-}
+
+
+
+
